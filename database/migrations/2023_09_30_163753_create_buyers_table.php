5469be67ed58bce577a38a8935e898f0d5965530
RicardoHubert <ricardohub681@gmail.com>
2023-09-30 23:45:53 +0700

    [DB] Menambahkan migrasi untuk db buyer

---
+++ database/migrations/2023_09_30_163753_create_buyers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuyersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buyers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('no_buyer');
+            $table->string('email');
+            $table->string('nama_perusahaan');
+            $table->string('alamat_perusahaan');
+            $table->string('product_interest')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buyers');
+    }
+}
