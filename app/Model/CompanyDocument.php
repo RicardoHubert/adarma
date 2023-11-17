4e71940c1009966ee2180786a4a61e557b9a7b57
RicardoHubert <ricardohub681@gmail.com>
2023-11-17 15:40:22 +0700

    [DB] Penambahan tabel untuk dokumen info company

---
+++ app/Model/CompanyDocument.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyDocument extends Model
+{
+    protected $guarded = ['id'];
+}
---
+++ database/migrations/2023_11_11_202935_create_company_documents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_doc');
+            $table->string('category_doc');
+            $table->string('file_doc');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_documents');
+    }
+}
